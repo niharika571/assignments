a488ee75893c0ce66789f640f165935876c82f32
niharika571 <71439752+niharika571@users.noreply.github.com>
2023-05-21 14:52:59 +0530

    Update question5.java

--- intermediate oops/question5.java
+++ intermediate oops/question5.java
@@ -1,1 +1,41 @@
 
+abstract class Shape {
+    abstract public void draw();
+}
+
+class Rectangle extends Shape {
+    @Override
+    public void draw() {
+        // Draw the rectangle
+        System.out.println("Drawing a rectangle");
+    }
+}
+
+class Line extends Shape {
+    @Override
+    public void draw() {
+        // Draw the line
+        System.out.println("Drawing a line");
+    }
+}
+
+class Cube extends Shape {
+    @Override
+    public void draw() {
+        // Draw the cube
+        System.out.println("Drawing a cube");
+    }
+}
+
+public class Assignment2Q5 {
+    public static void main(String[] args) {
+        Shape rectangle = new Rectangle();
+        rectangle.draw();
+
+        Shape line = new Line();
+        line.draw();
+
+        Shape cube = new Cube();
+        cube.draw();
+    }
+}
